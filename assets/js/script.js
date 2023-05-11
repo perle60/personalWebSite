2482c862d114010f0280fa5c1343920ae2dfb224
aenzo <enzo.achard@ynov.com>
2023-05-11 22:28:09 +0200

    refonte

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function copyToClipboard(text) {
+    var dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+    alert('L\'adresse e-mail a été copiée dans le presse-papiers.');
+  }
